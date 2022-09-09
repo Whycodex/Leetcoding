c65b1b2afe67ebe2ec09462fb693dbc88090dbe2
Whycodex <96807353+Whycodex@users.noreply.github.com>
2022-09-09 11:43:54 +0530

    Time: 1707 ms (5.05%), Space: 125.1 MB (91.78%) - LeetHub

---
+++ 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    static bool cmp(vector<int>&a,vector<int>&b){
+        if(a[0]==b[0])
+            return a[1]>b[1];
+        return a[0]<b[0];
+    }
+    int numberOfWeakCharacters(vector<vector<int>>& properties) {
+        sort(properties.begin(),properties.end(),cmp);
+        int res=0;
+        int mx=INT_MIN;
+        for(int i=properties.size()-1;i>=0;i--){
+            if(properties[i][1]<mx)
+                res++;
+            mx=max(mx,properties[i][1]);
+        }
+        return res;
+    }
+};
